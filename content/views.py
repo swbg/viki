fc6be20683ccc35401e52f65a959933529364eb3
swbg <weissenberger.stefan@gmx.net>
2017-03-08 15:04:00 +0100

    viki-02

--- content/views.py
+++ content/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.utils import timezone
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(updated__lte=timezone.now()).order_by('id')
+    return render(request, 'content/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'content/post_detail.html', {'post': post})
